d0fd2c91d848371921ec53bf16edac9f2ace9217
xudong <dongxu222mk@163.com>
2020-12-27 22:43:26 +0800

    更新preprocess.py 2020/12/27

--- text_classification/preprocess.py
+++ text_classification/preprocess.py
@@ -8,7 +8,6 @@
 import os
 import _pickle as pickle
 import pandas as pd
-import jieba
 import random
 
 from sklearn.model_selection import train_test_split
@@ -19,14 +18,10 @@ from sklearn.model_selection import train_test_split
 """
 
 # 可以人为自定义label dict
-label_dict_default = {108: 0, 104: 1, 106: 2, 112: 3,
-                      109: 4, 103: 5, 116: 6, 101: 7,
-                      107: 8, 100: 9, 102: 10, 110: 11,
-                      115: 12, 113: 13, 114: 14}
-
-emotion_dict_default = {'sadness': 0, 'anger': 1,
-                        'happiness': 2, 'fear': 3,
-                        'like': 4, 'disgust': 5, 'surprise': 6}
+label_dict_default = {109: 0, 104: 1, 102: 2, 113: 3,
+                      107: 4, 101: 5, 103: 6, 110: 7,
+                      108: 8, 116: 9, 112: 10, 115: 11,
+                      106: 12, 100: 13, 114: 14}
 
 
 def make_vocab(file_path):
@@ -35,29 +30,22 @@ def make_vocab(file_path):
     :param file_path:
     :return:
     """
-    data = pd.read_csv(file_path, sep='\t', header=None)
-    data.columns = ['id', 'text', 'label']
+    data = pd.read_csv(file_path, sep='\t')
     vocab = {'PAD': 0, 'UNK': 1}
     words_list = []
-    label_dict = {}
     for index, row in data.iterrows():
-        # todo 也可以使用分词软件进行分词
-        text = jieba.lcut(row['text'])
         label = row['label']
-        if label not in label_dict:
-            label_dict[label] = len(label_dict)
-        for word in text:
+        words = row['words'].split(' ')
+        for word in words:
             words_list.append(word)
     random.shuffle(words_list)
     for word in words_list:
         if word not in vocab:
             vocab[word] = len(vocab)
     # save to file and print the label dict
-    save_path = './vocab.txt'
+    save_path = './data_path/vocab.txt'
     save_vocab(vocab, save_path)
-    print(vocab)
     print(f'the vocab size is {len(vocab)}')
-    print(f'the label dict is : {label_dict}')
     return vocab
 
 
@@ -68,17 +56,15 @@ def make_data(file_path, vocab, type):
     :param vocab
     :return:
     """
-    data = pd.read_csv(file_path, sep='\t', header=None)
-    data.columns = ['id', 'text', 'label']
+    data = pd.read_csv(file_path, sep='\t')
     word_ids = []
     label_ids = []
     for index, row in data.iterrows():
-        # todo 也可以使用分词软件进行分词，然后基于词粒度来做
-        text = jieba.lcut(row['text'])
         label = row['label']
-        word_id_temp = [vocab.get(word) if word in vocab else 1 for word in text]
+        words = row['words'].split(' ')
+        word_id_temp = [vocab.get(word) if word in vocab else 1 for word in words]
         word_ids.append(word_id_temp)
-        label_ids.append(emotion_dict_default.get(label))
+        label_ids.append(label_dict_default.get(label))
 
     print(f'the {type} data size is {len(word_ids)}')
     print(word_ids[0])
@@ -107,8 +93,8 @@ def main(output_path):
     :return:
     """
     data = {}
-    train_path = './data_path/train_emotion.csv'
-    test_path = './data_path/test_motion.csv'
+    train_path = './data_path/train_data.csv'
+    test_path = './data_path/dev_data.csv'
     vocab = make_vocab(train_path)
     train_data = make_data(train_path, vocab, 'train')
     test_data = make_data(test_path, vocab, 'test')
@@ -116,7 +102,7 @@ def main(output_path):
     data['train'] = train_data
     data['test'] = test_data
 
-    data_path = os.path.join(output_path, 'emotion_data.pkl')
+    data_path = os.path.join(output_path, 'tnews_data.pkl')
     pickle.dump(data, open(data_path, 'wb'), protocol=2)
     print('save data to pkl over.')
 
@@ -144,6 +130,4 @@ def split_data(file_path, output):
 
 if __name__ == '__main__':
     output_path = './data_path'
-    # main(output_path)
-    # todo filter 一些特殊字符，and 停用词表
-    split_data('./data_path/OCNLI_train1128.csv', './data_path/')
+    main(output_path)
